f6d595cab540d5458a6e257601feb53a133d1201
Артем Томащук <tomashhukartem@gmail.com>
2024-09-06 19:37:26 +0300

    deploy new code

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,34 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+document.querySelector('.form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const delayInput = event.target.elements.delay.value;
+  const state = event.target.elements.state.value;
+  const delay = parseInt(delayInput, 10);
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
+});
